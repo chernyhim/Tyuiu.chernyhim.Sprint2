2100cff3043913869f1daf52c25ec2866d2d6372
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-09-29 12:30:43 +0500

    Task1

--- Tyuiu.chernyhim.Sprint2.Task1.V11/Program.cs
+++ Tyuiu.chernyhim.Sprint2.Task1.V11/Program.cs
@@ -3,5 +3,17 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        DataService ds = new DataService();
+        Console.WriteLine("Введите A");
+        int x, y, z, w;
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите B");
+        y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите C");
+        z = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите D");
+        w = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(ds.GetLogicOperations(x, y, z, w));
     }
 }
